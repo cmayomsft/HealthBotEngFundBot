ee9806c9a24ac80148780b1f3ce3a7ff2f02244c
Chris Mayo <cmayo@microsoft.com>
2020-04-15 13:54:22 -0700

    Updated to support set and get, tested.

--- getScenarios.ps1
+++ getScenarios.ps1
@@ -5,4 +5,4 @@ Set-PsEnv
 
 Import-Module -Name ./Scripts/ScenarioImportExport/HCBotScenarioIE/;
 # Export Scenarios from the bot instance specified in .env variables to the ./Scenarios folder.
-Get-HCBScenario -Tenant $env:SOURCE_TENANT_NAME -SaveToFolder ./Scenarios -ParseCode -JwtSecret $env:SOURCE_JWT_SECRET -InformationAction Continue;
+Get-HCBScenario -Tenant $env:MY_DEV_TENANT_NAME -SaveToFolder ./Scenarios -ParseCode -JwtSecret $env:MY_DEV_JWT_SECRET -InformationAction Continue;
--- setScenarios.ps1
+++ setScenarios.ps1
@@ -5,4 +5,4 @@ Set-PsEnv
 
 Import-Module -Name ./Scripts/ScenarioImportExport/HCBotScenarioIE/;
 # Export Scenarios from the bot instance specified in .env variables to the ./Scenarios folder.
-Set-HCBScenario -FromFolder ./Scenarios -Tenant $env:SOURCE_TENANT_NAME -JwtSecret $env:SOURCE_JWT_SECRET -InformationAction Continue;
+Set-HCBScenario -FromFolder ./Scenarios -Tenant $env:MY_DEV_TENANT_NAME -JwtSecret $env:MY_DEV_JWT_SECRET -InformationAction Continue;
